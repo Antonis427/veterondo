302b12eedc0f2b5da80989ef7c4d4321409907a0
Antonis Tsagaris <Antonis Tsagaris>
2016-05-01 21:46:33 +0300

    start implementing daydream functionality

---
+++ app/src/main/java/com/saladdressing/veterondo/services/Daydream.java
@@ -0,0 +1,29 @@
+package com.saladdressing.veterondo.services;
+
+import android.service.dreams.DreamService;
+
+/**
+ * Created by sebas on 5/1/2016.
+ */
+public class Daydream extends DreamService {
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    public void onDreamingStarted() {
+        super.onDreamingStarted();
+    }
+
+    @Override
+    public void onDreamingStopped() {
+        super.onDreamingStopped();
+    }
+}
